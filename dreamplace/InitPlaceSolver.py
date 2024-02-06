1cd39d992b0d6d856119697c7d89b35e67636171
JuneTech <junetech0@gmail.com>
2024-02-06 20:53:42 +0900

    math model 4 test

--- dreamplace/InitPlaceSolver.py
+++ dreamplace/InitPlaceSolver.py
@@ -12,7 +12,7 @@ from dreamplace.PlaceDB import PlaceDB
 
 def make_model(placedb: PlaceDB) -> Model:
     large_movable_node_area_criterion = 10000
-    large_fixed_node_area_criterion = 100000
+    large_fixed_node_area_criterion = 10000
     mdl = Model("Diet")
 
     # Parameters start
@@ -74,16 +74,13 @@ def make_model(placedb: PlaceDB) -> Model:
     p_prime_set = np.concatenate(
         [placedb.node2pin_map[n_id] for n_id in v_prime_set], axis=None
     )
-    e_prime_set = {
-        net_id: np.intersect1d(placedb.net2pin_map[net_id], p_prime_set)
-        for net_id in range(len(placedb.net2pin_map))
-        if np.any(np.intersect1d(placedb.net2pin_map[net_id], p_prime_set))
-    }
-    # e_prime_set = {}
-    # for net_id in range(len(placedb.net2pin_map)):
-    #     pins = np.intersect1d(placedb.net2pin_map[net_id], p_prime_set)
-    #     if np.any(pins):
-    #         e_prime_set[net_id] = pins
+    e_prime_set = {}
+    for net_id, pin_in_a_net in enumerate(placedb.net2pin_map):
+        bool_list = np.isin(p_prime_set, pin_in_a_net, assume_unique=True)
+        if bool_list.sum() <= 1:
+            continue
+        e_prime_set[net_id] = np.intersect1d(placedb.net2pin_map[net_id], p_prime_set)
+
     print("Parameter definition end")
     # Parameters end
 
@@ -98,51 +95,6 @@ def make_model(placedb: PlaceDB) -> Model:
     print(f"{len(x_var)} x variables, {len(y_var)} y variables declared")
     print("Variables definition end")
 
-    # Constraints start
-    # selected cells should not be placed outside placement area
-    for n_id in sel_mv_n_id:
-        mdl.addCons(x[n_id] <= block_wth, name="XUB(%s)" % n_id)
-        mdl.addCons(x[n_id] + n_wth[n_id] >= 0, name="XLB(%s)" % n_id)
-        mdl.addCons(y[n_id] <= block_hgt, name="YUB(%s)" % n_id)
-        mdl.addCons(y[n_id] + n_hgt[n_id] >= 0, name="YLB(%s)" % n_id)
-    print("Constraints 1 definition end")
-    # there is no overlap among selected movable cells and fixed cells
-    big_M_x, big_M_y = block_wth, block_hgt
-    delta: Dict[int, Dict[int, Dict[int, Variable]]] = {}
-    for u in sel_mv_n_id:
-        delta = {u: {}}
-        for v in v_prime_set:
-            if u == v:
-                continue
-            delta[u] = {
-                v: {
-                    k: mdl.addVar(vtype="B", name="delta(%s,%s,%s)" % (u, v, k))
-                    for k in range(1, 5)
-                }
-            }
-            mdl.addCons(
-                x[u] <= -n_wth[u] + x[v] + big_M_x * delta[u][v][1],
-                name="Ovlp(%s,%s,%s)" % (u, v, 1),
-            )
-            mdl.addCons(
-                x[u] >= x[v] + n_wth[v] - big_M_x * delta[u][v][2],
-                name="Ovlp(%s,%s,%s)" % (u, v, 2),
-            )
-            mdl.addCons(
-                y[u] <= -n_hgt[u] + y[v] + big_M_y * delta[u][v][3],
-                name="Ovlp(%s,%s,%s)" % (u, v, 3),
-            )
-            mdl.addCons(
-                y[u] >= y[v] + n_hgt[v] - big_M_y * delta[u][v][4],
-                name="Ovlp(%s,%s,%s)" % (u, v, 4),
-            )
-            mdl.addCons(
-                quicksum(delta[u][v][k] for k in range(1, 5)) <= 3,
-                name="OvlpCnt(%s,%s)" % (u, v),
-            )
-    print(f"{sum(len(val) for val in delta.values())} delta variables declared")
-    print("Constraints 2 definition end")
-    # Constraints end
     # Objective
     obj_term: Dict[int, Dict[int, Variable]] = {}
     for pins in e_prime_set.values():
@@ -180,7 +132,7 @@ def make_model(placedb: PlaceDB) -> Model:
         quicksum(quicksum(obj_term[p].values()) for p in obj_term), "minimize"
     )
     print("Objective definition end")
-    mdl.data = x_var, y_var, delta
+    mdl.data = x_var, y_var
     mdl.setParam("limits/gap", 0.1)  # stop when optimality gap >= 10%
 
     return mdl
@@ -190,7 +142,7 @@ def return_sol(mdl: Model) -> Tuple[Dict[int, float], Dict[int, float]]:
     mdl.optimize()
     if mdl.getStatus() == "infeasible":
         return {}, {}
-    x, y, _ = mdl.data
+    x, y = mdl.data
     x_dict = {v: mdl.getVal(x[v]) for v in x}
     y_dict = {v: mdl.getVal(y[v]) for v in y}
     return x_dict, y_dict
