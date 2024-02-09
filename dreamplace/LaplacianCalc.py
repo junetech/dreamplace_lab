23b74fa152c408c287334b10d807173625166f48
JuneTech <junetech0@gmail.com>
2024-02-09 16:31:17 +0900

    Numba Laplacian matrix calc test was not good

--- dreamplace/LaplacianCalc.py
+++ dreamplace/LaplacianCalc.py
@@ -1,14 +1,28 @@
-import numpy as onp
-import jax.numpy as jnp
+import numba as nb
+import numpy as np
 
 
+@nb.jit(parallel=True)
+def isin(a, b):
+    """
+    https://stackoverflow.com/questions/70865732/faster-numpy-isin-alternative-for-strings-using-numba
+    """
+    out = np.empty(a.shape[0], dtype=nb.boolean)
+    b = set(b)
+    for i in nb.prange(a.shape[0]):
+        if a[i] in b:
+            out[i] = True
+        else:
+            out[i] = False
+    return out
+
+
+@nb.jit(parallel=True)
 def calc_laplacian(node_list, edge_matrix):
-    jax_nodes = jnp.array(node_list)
-    jax_edges = jnp.array(edge_matrix)
-    l_matrix = jnp.zeros((len(jax_nodes), len(jax_nodes)))
-    for nodes_in_an_edge in jax_edges:
-        bool_list = jnp.isin(jax_nodes, nodes_in_an_edge, assume_unique=True)
-        l_matrix += jnp.diag(bool_list) * bool_list.sum() - jnp.outer(
+    l_matrix = np.zeros((len(node_list), len(node_list)))
+    for pins_in_a_net in edge_matrix:
+        bool_list = isin(node_list, pins_in_a_net)
+        l_matrix += np.diag(bool_list) * bool_list.sum() - np.outer(
             bool_list, bool_list
         )
-    return onp.array(l_matrix)
+    return l_matrix
