e2ca7c2d04be509bf67fc312b5f29feaf401ddf1
JuneTech <junetech0@gmail.com>
2024-02-09 16:34:28 +0900

    reverting to the working code

--- dreamplace/LaplacianCalc.py
+++ dreamplace/LaplacianCalc.py
@@ -1,27 +1,10 @@
-import numba as nb
 import numpy as np
 
 
-@nb.jit(parallel=True)
-def isin(a, b):
-    """
-    https://stackoverflow.com/questions/70865732/faster-numpy-isin-alternative-for-strings-using-numba
-    """
-    out = np.empty(a.shape[0], dtype=nb.boolean)
-    b = set(b)
-    for i in nb.prange(a.shape[0]):
-        if a[i] in b:
-            out[i] = True
-        else:
-            out[i] = False
-    return out
-
-
-@nb.jit(parallel=True)
 def calc_laplacian(node_list, edge_matrix):
     l_matrix = np.zeros((len(node_list), len(node_list)))
     for pins_in_a_net in edge_matrix:
-        bool_list = isin(node_list, pins_in_a_net)
+        bool_list = np.isin(node_list, pins_in_a_net, assume_unique=True)
         l_matrix += np.diag(bool_list) * bool_list.sum() - np.outer(
             bool_list, bool_list
         )
