71775c9fffce108f3106af2be1da1774b62f2dd1
JuneTech <junetech0@gmail.com>
2024-02-13 16:08:09 +0900

    calc acceleration by JAX

--- dreamplace/LaplacianCalc.py
+++ dreamplace/LaplacianCalc.py
@@ -1,11 +1,11 @@
-import numpy as np
+import jax.numpy as jnp
 
 
 def calc_laplacian(node_list, edge_matrix):
-    l_matrix = np.zeros((len(node_list), len(node_list)))
+    l_matrix = jnp.zeros((len(node_list), len(node_list)))
     for pins_in_a_net in edge_matrix:
-        bool_list = np.isin(node_list, pins_in_a_net, assume_unique=True)
-        l_matrix += np.diag(bool_list) * bool_list.sum() - np.outer(
+        bool_list = jnp.isin(node_list, pins_in_a_net, assume_unique=True)
+        l_matrix += jnp.diag(bool_list) * bool_list.sum() - jnp.outer(
             bool_list, bool_list
         )
     return l_matrix
