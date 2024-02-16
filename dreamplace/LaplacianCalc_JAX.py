0d0c896744def442ef074a52721f35cdbcc7e3f7
JuneTech <junetech0@gmail.com>
2024-02-16 18:51:33 +0900

    Unable to do this operation in JAX

--- dreamplace/LaplacianCalc_JAX.py
+++ dreamplace/LaplacianCalc_JAX.py
@@ -6,7 +6,7 @@ import jax.numpy as jnp
 
 
 def calc_laplacian(pin_list, edge_matrix):
-    pin_id2idx: dict[int, int] = {pin_id: idx for idx, pin_id in enumerate(pin_list)}
+    # pin_id2idx: dict[int, int] = {pin_id: idx for idx, pin_id in enumerate(pin_list)}
     pin_count = pin_list.size
     l_matrix = jnp.zeros((pin_count, pin_count), dtype=jnp.int16)
     for pins_in_a_net in edge_matrix:
@@ -14,9 +14,10 @@ def calc_laplacian(pin_list, edge_matrix):
         degree_plus_one = len(pins_of_interest)
         if degree_plus_one <= 1:
             continue
-        for u, v in combinations(pins_of_interest, 2):
-            l_matrix[pin_id2idx[u]][pin_id2idx[v]] -= 1
-            l_matrix[pin_id2idx[v]][pin_id2idx[u]] -= 1
-        for pin in pins_of_interest:
-            l_matrix[pin_id2idx[pin]][pin_id2idx[pin]] += degree_plus_one - 1
+        pin_indices = [jnp.where(pin_list == pin) for pin in pins_of_interest]
+        for u, v in combinations(pin_indices, 2):
+            l_matrix.at[u, v].set(l_matrix.at[u, v] - 1)
+            l_matrix.at[v, u].set(l_matrix.at[v, u] - 1)
+        for pin in pin_indices:
+            l_matrix.at[pin, pin].set(l_matrix.at[pin, pin] - 1 + degree_plus_one)
     return l_matrix
