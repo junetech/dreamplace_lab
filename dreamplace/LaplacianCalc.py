76e2dc01ee234520a3330a5752598212c67fcf2f
JuneTech <junetech0@gmail.com>
2024-02-16 16:01:58 +0900

    another Laplacian calculation

--- dreamplace/LaplacianCalc.py
+++ dreamplace/LaplacianCalc.py
@@ -1,6 +1,7 @@
 """Laplacian Matrix calculation using numpy
 """
 
+from itertools import combinations
 import numpy as np
 
 
@@ -11,10 +12,17 @@ def calc_laplacian(node_list, edge_matrix):
     # for idx, pins_in_a_net in enumerate(edge_matrix):
     for pins_in_a_net in edge_matrix:
         # print(idx, "/", edge_count)
-        bool_list = np.isin(node_list, pins_in_a_net, assume_unique=True)
-        if bool_list.sum() <= 1:
-            continue
-        l_matrix += np.diag(bool_list) * bool_list.sum() - np.outer(
-            bool_list, bool_list
-        )
+        # bool_list = np.isin(node_list, pins_in_a_net, assume_unique=True)
+        # if bool_list.sum() <= 1:
+        #     continue
+        # l_matrix += np.diag(bool_list) * bool_list.sum() - np.outer(
+        #     bool_list, bool_list
+        # )
+        pins_of_interest = np.intersect1d(pins_in_a_net, node_list)
+        if len(pins_of_interest) >= 2:
+            for u, v in combinations(pins_of_interest, 2):
+                l_matrix[u][v] -= 1
+                l_matrix[v][u] -= 1
+            for pin in pins_of_interest:
+                l_matrix[pin][pin] += 1
     return l_matrix
