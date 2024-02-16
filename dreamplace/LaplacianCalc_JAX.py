6b192e4f16861f536c4fb91434a00efba3cd2975
JuneTech <junetech0@gmail.com>
2024-02-16 22:09:08 +0900

    Tried minimum JAX application but failed

--- dreamplace/LaplacianCalc_JAX.py
+++ dreamplace/LaplacianCalc_JAX.py
@@ -3,19 +3,20 @@
 
 from itertools import combinations
 import numpy as np
+import jax.numpy as jnp
 
 
 def calc_laplacian(pin_list, edge_matrix):
     pin_id2idx: dict[int, int] = {pin_id: idx for idx, pin_id in enumerate(pin_list)}
     pin_count = pin_list.size
-
+    _pin_list = jnp.array(pin_list)
     l_matrix = np.zeros((pin_count, pin_count), dtype=np.int16)
     for pins_in_a_net in edge_matrix:
-        pins_of_interest = np.intersect1d(pins_in_a_net, pin_list)
+        pins_of_interest = jnp.intersect1d(pins_in_a_net, _pin_list)
         degree_plus_one = len(pins_of_interest)
         if degree_plus_one <= 1:
             continue
-        pin_indices = [pin_id2idx[pin] for pin in pins_of_interest]
+        pin_indices = [pin_id2idx[pin] for pin in pins_of_interest.astype(np.int16)]
         for p, q in combinations(pin_indices, 2):
             l_matrix[p][q] -= 1
             l_matrix[q][p] -= 1
